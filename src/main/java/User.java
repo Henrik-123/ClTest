48947e344a0b3e7415ca5b5d39537550ed5d5839
henrikbaidori <Henrik_Baidori@hotmail.com>
2025-03-31 15:12:39 +0200

    Initial commit

---
+++ src/main/java/User.java
@@ -0,0 +1,22 @@
+public class User {
+
+    private String userName;
+    private String password;
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void changeName(String newName) {
+        userName = newName;
+    }
+}
---
+++ src/test/java/UserTest.java
@@ -0,0 +1,47 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UserTest {
+
+
+
+    @Test
+    public void testCreateUser() {
+        User user = new User("Henke","asd123");
+    }
+
+    @Test
+    public void testGetUserName() {
+        User user = new User("Henke","asd123");
+
+        String actual = user.getName();
+        String expected = "Henke";
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testGetPassword() {
+        User user = new User("Henke","asd123");
+
+        String actual = user.getPassword();
+        String expected = "asd123";
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testChangeUserName() {
+        User user = new User("Henke","asd123");
+
+        user.changeName("Erik");
+
+        String actual = user.getName();
+        String expected = "Erik";
+
+        assertEquals(expected, actual);
+
+    }
+
+}
